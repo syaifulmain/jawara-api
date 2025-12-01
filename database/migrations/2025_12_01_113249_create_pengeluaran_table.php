47da72728c05808d3432dd1407eadb3bd2a94e02
satrio wisnu <satriowisnuap@gmail.com>
2025-12-01 18:52:34 +0700

    fix migration table name from 't_pengeluaran' to 'pengeluaran'

--- database/migrations/2025_12_01_113249_create_pengeluaran_table.php
+++ database/migrations/2025_12_01_113249_create_pengeluaran_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('t_pengeluaran', function (Blueprint $table) {
+        Schema::create('pengeluaran', function (Blueprint $table) {
             $table->id();
 
             // Jenis pengeluaran 
